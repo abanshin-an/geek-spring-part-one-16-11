421d621111f4eed88750bfca6fb969c47c19e28c
abanshin-an <abanshin-an@utg.gazprom.ru>
2021-12-19 00:48:40 +0400

    lesson1

--- lesson-01-servlet-api/src/main/java/ru/geekbrains/ProductServlet.java
+++ lesson-01-servlet-api/src/main/java/ru/geekbrains/ProductServlet.java
@@ -14,6 +14,9 @@ import java.io.PrintWriter;
 @WebServlet(urlPatterns = "/product/*")
 public class ProductServlet extends HttpServlet {
 
+    private static final String TR = "</tr>";
+    private static final String TR1 = "<tr>";
+    private static final String TD = "</td>";
     private ProductRepository productRepository;
 
     @Override
@@ -22,27 +25,43 @@ public class ProductServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, NumberFormatException {
         PrintWriter wr = resp.getWriter();
         if (req.getParameter("id") != null) {
-            // TODO
+            long id = Long.parseLong(req.getParameter("id"));
+            Product product = productRepository.findById(id);
+            if (product == null) {
+                wr.println("Product id=" + id + " not found");
+            } else {
+                wr.println("<table>");
+                wr.println(TR1);
+                wr.println("<th>Name</th>");
+                wr.println("<th>Value</th>");
+                wr.println(TR);
+                wr.println(TR1);
+                wr.println("<td>  id </td>");
+                wr.println("<td> " + product.getId() + TD);
+                wr.println(TR);
+                wr.println(TR1);
+                wr.println("<td>  name </td>");
+                wr.println("<td>" + product.getName() + TD);
+                wr.println(TR);
+                wr.println("</table>");
+            }
         } else if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
             wr.println("<table>");
-
-            wr.println("<tr>");
+            wr.println(TR1);
             wr.println("<th>Id</th>");
             wr.println("<th>Name</th>");
-            wr.println("</tr>");
-
+            wr.println(TR);
             for (Product product : productRepository.findAll()) {
-                wr.println("<tr>");
-                wr.println("<td>" + product.getId() + "</td>");
-                wr.println("<td>" + product.getName() + "</td>");
-                // TODO <a href='product?id=12'></a>
-                wr.println("</tr>");
+                wr.println(TR1);
+                wr.println("<td>" + product.getId() + TD);
+                wr.println("<td><a href='product?id=" + product.getId() + "'>" + product.getName() + "</a></td>");
+                wr.println(TR);
             }
-
             wr.println("</table>");
         }
     }
+
 }
