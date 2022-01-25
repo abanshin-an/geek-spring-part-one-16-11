7f16aa2d950908e7e527de421a28313272cd2a8d
abanshin-an <abanshin-an@utg.gazprom.ru>
2022-01-25 20:12:36 +0400

    lesson 8

---
+++ lesson-08-thymeleaf/src/main/java/ru/geekbrains/service/ProductServiceImpl.java
@@ -0,0 +1,69 @@
+package ru.geekbrains.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import ru.geekbrains.persist.*;
+import ru.geekbrains.service.dto.ProductDto;
+
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository,
+                              CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public Page<ProductDto> findAll(Specification<Product> spec, Integer page,
+                                    Integer size, Sort sort) {
+        return productRepository.findAll(spec, PageRequest.of(page, size, sort))
+                .map(ProductServiceImpl::convertToDto);
+    }
+
+    @Override
+    public Optional<ProductDto> findById(Long id) {
+        return productRepository.findById(id)
+                .map(ProductServiceImpl::convertToDto);
+    }
+
+    @Override
+    public ProductDto save(ProductDto productDto) {
+        Category category = categoryRepository.findById(productDto.getCategoryId())
+                .orElse(null);
+        Product product = new Product(
+                productDto.getId(),
+                productDto.getName(),
+                productDto.getDescription(),
+                productDto.getPrice(), category);
+        Product saved = productRepository.save(product);
+        return convertToDto(saved);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    private static ProductDto convertToDto(Product prod) {
+        return new ProductDto(
+                prod.getId(),
+                prod.getName(),
+                prod.getDescription(),
+                prod.getPrice(),
+                prod.getCategory() != null ? prod.getCategory().getId() : null,
+                prod.getCategory() != null ? prod.getCategory().getName() : null
+        );
+    }
+}
